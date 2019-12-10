f70ad4900af112eee32e7e494c9e400d9fe60883
SoulJa Zhao <long.zhao@bhpbilliton.com>
2019-12-10 13:28:21 +0800

    add CalculatorButtonItem Model

---
+++ Calculator/Calculator/CalculatorButtonItem.swift
@@ -0,0 +1,53 @@
+//
+//  CalculatorButtonItem.swift
+//  Calculator
+//
+//  Created by Zhao, Long on 2019/12/10.
+//  Copyright © 2019 Zhao, Long. All rights reserved.
+//
+
+import UIKit
+
+enum CalculatorButtonItem {
+    enum Op: String {
+        case plus = "+"
+        case minus = "-"
+        case divide = "÷"
+        case multiply = "×"
+        case equal = "="
+    }
+    
+    enum Command: String {
+        case clear = "AC"
+        case flip = "+/-"
+        case percent = "%"
+    }
+    
+    case digit(Int)
+    case dot
+    case op(Op)
+    case command(Command)
+}
+
+extension CalculatorButtonItem {
+    var title: String {
+        switch self {
+        case .digit(let value): return String(value)
+        case .dot: return "."
+        case .op(let op): return op.rawValue
+        case .command(let command): return command.rawValue
+        }
+    }
+    
+    var size: CGSize {
+        CGSize(width: 88, height: 88)
+    }
+    
+    var backgroundColorName: String {
+        switch self {
+        case .digit, .dot: return "digitBackground"
+        case .op: return "operatorBackground"
+        case .command: return "commandBackground"
+        }
+    }
+}
