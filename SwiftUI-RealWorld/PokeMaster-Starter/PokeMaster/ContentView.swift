ce3ba4b9624f1846706fd35557fc0cf699af1021
SoulJa Zhao <long.zhao@bhpbilliton.com>
2019-12-12 11:21:19 +0800

    add animation to cell

--- SwiftUI-RealWorld/PokeMaster-Starter/PokeMaster/ContentView.swift
+++ SwiftUI-RealWorld/PokeMaster-Starter/PokeMaster/ContentView.swift
@@ -10,12 +10,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        PokemonInfoRow()
+        VStack {
+            PokemonInfoRow(model: PokemonViewModel.sample(id: 1), expanded: false)
+            PokemonInfoRow(model: PokemonViewModel.sample(id: 21), expanded: true)
+            PokemonInfoRow(model: PokemonViewModel.sample(id: 25), expanded: false)
+        }
     }
 }
 
 struct PokemonInfoRow: View {
-    let model = PokemonViewModel.sample(id: 1)
+    let model:PokemonViewModel
+    @State var expanded:Bool
     
     var body: some View {
         VStack {
@@ -37,37 +42,62 @@ struct PokemonInfoRow: View {
                 }
             }
             .padding(.top, 12)
-            
-            HStack {
+            Spacer()
+            HStack(spacing: expanded ? 20 : -30) {
                 Spacer()
                 Button(action: {
                     print("fav")
                 }) {
                     Image(systemName: "star")
-                        .font(.system(size: 25))
-                        .foregroundColor(.white)
-                        .frame(width: 30, height: 30, alignment: .center)
+                    .modifier(ToolButtonModifier())
                 }
                 Button(action: {
                     print("panel")
                 }) {
                     Image(systemName: "chart.bar")
-                        .font(.system(size: 25))
-                        .foregroundColor(.white)
-                        .frame(width: 30, height: 30, alignment: .center)
+                    .modifier(ToolButtonModifier())
                 }
                 Button(action: {
                     print("web")
                 }) {
                     Image(systemName: "info.circle")
-                        .font(.system(size: 25))
-                        .foregroundColor(.white)
-                        .frame(width: 30, height: 30, alignment: .center)
+                    .modifier(ToolButtonModifier())
                 }
             }
             .padding(.bottom, 12)
+            .opacity(expanded ? 1.0 : 0.0)
+            .frame(maxHeight: expanded ? .infinity : 0)
         }
-        .background(Color.green)
+        .frame(height: expanded ? 120 : 80)
+        .padding(.leading, 23)
+        .padding(.trailing, 15)
+        .background(
+            ZStack {
+                RoundedRectangle(cornerRadius: 20).stroke(model.color, lineWidth: 4.0)
+                RoundedRectangle(cornerRadius: 20).fill(
+                    LinearGradient(
+                            gradient: Gradient(colors: [.white, model.color]),
+                            startPoint: .leading,
+                            endPoint: .trailing
+                          )
+                )
+            }
+        )
+        .padding(.horizontal)
+        .animation(
+            Animation.spring(response: 0.55, dampingFraction: 0.425, blendDuration: 0.0)
+        )
+        .onTapGesture {
+            self.expanded.toggle()
+        }
+    }
+}
+
+struct ToolButtonModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content.font(.system(size: 25))
+            .foregroundColor(.white)
+            .frame(width: 30, height: 30, alignment: .center)
     }
 }
 
