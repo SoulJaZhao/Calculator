271f1a02384606163f03b7ea4dd82500841ab326
SoulJaZhao <superzhaolong@126.com>
2019-12-31 21:42:20 +0800

    test several combine methods

--- CombineExample-Starter.playground/Pages/Basic.xcplaygroundpage/Contents.swift
+++ CombineExample-Starter.playground/Pages/Basic.xcplaygroundpage/Contents.swift
@@ -1,4 +1,5 @@
 import Combine
+import Foundation
 
 check("Empty") {
     Empty<Int, SampleError>()
@@ -34,3 +35,58 @@ check("Catch and Continue") {
         }
     }
 }
+
+check("Filter") {
+    [1, 2, 3, 4, 5].publisher.filter { (i) -> Bool in
+        return i % 2 == 0
+    }
+}
+
+check("contain") {
+    [1, 2, 3, 4, 5].publisher.print("[Original]").contains(10)
+}
+
+let subject_example1 = PassthroughSubject<Int, Never>()
+let subject_example2 = PassthroughSubject<Int, Never>()
+
+check("Subject Order") {
+  subject_example1.merge(with: subject_example2)
+}
+
+subject_example1.send(20)
+subject_example2.send(1)
+subject_example1.send(40)
+subject_example1.send(60)
+subject_example2.send(1)
+subject_example1.send(80)
+subject_example1.send(100)
+subject_example1.send(completion: .finished)
+subject_example2.send(completion: .finished)
+
+struct Response: Decodable {
+  struct Args: Decodable {
+    let foo: String
+  }
+  let args: Args?
+}
+
+check("URLSession") {
+    URLSession.shared.dataTaskPublisher(for: URL.init(string: "https://httpbin.org/get?foo=bar")!).map { (data, _) in
+        data
+    }.decode(type: Response.self, decoder: JSONDecoder.init()).compactMap {
+        $0.args?.foo
+    }
+}
+
+class Wrapper {
+    @Published var text: String = "hoho"
+}
+
+var wrapper = Wrapper.init()
+
+check("@Published") {
+    wrapper.$text
+}
+
+wrapper.text = "123"
+wrapper.text = "abc"
